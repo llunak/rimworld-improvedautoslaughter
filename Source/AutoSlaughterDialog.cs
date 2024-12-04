7024febbf981d07b743bb138d8f44b00c89a299e
Luboš Luňák <l.lunak@centrum.cz>
2024-12-04 19:30:29 +0100

    change all the related visible strings in the dialog

--- Source/AutoSlaughterDialog.cs
+++ Source/AutoSlaughterDialog.cs
@@ -18,27 +18,61 @@ namespace ImprovedAutoSlaughter
         }
 
         [HarmonyTranspiler]
+        [HarmonyPatch(nameof(DoWindowContents))]
+        public static IEnumerable<CodeInstruction> DoWindowContents(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+            bool found = false;
+            for( int i = 0; i < codes.Count; ++i )
+            {
+                // Log.Message("T:" + i + ":" + codes[i].opcode + "::" + (codes[i].operand != null ? codes[i].operand.ToString() : codes[i].operand));
+                if( codes[ i ].opcode == OpCodes.Ldstr && codes[ i ].operand.ToString() == "AutoSlaugtherTip" )
+                {
+                    codes[ i ] = new CodeInstruction( OpCodes.Ldstr, "ImprovedAutoSlaughter.Tip" );
+                    found = true;
+                    // There are more instances, do all.
+                }
+            }
+            if(!found)
+                Log.Error("ImprovedAutoSlaughter: Failed to patch Dialog_AutoSlaughter.DoWindowContents()");
+            return codes;
+        }
+
+        [HarmonyTranspiler]
         [HarmonyPatch(nameof(DoAnimalHeader))]
         public static IEnumerable<CodeInstruction> DoAnimalHeader(IEnumerable<CodeInstruction> instructions)
         {
             var codes = new List<CodeInstruction>(instructions);
-            bool found1 = false;
-            bool found2 = false;
+            var replace =
+            new[] {
+                ( source: "AnimalMaleYoung", replace: "ImprovedAutoSlaughter.AnimalMaleTotal", done: false ),
+                ( source: "AnimalFemaleYoung", replace: "ImprovedAutoSlaughter.AnimalFemaleTotal", done: false ),
+                ( source: "AutoSlaughterHeaderTooltipCurrentMalesYoung", replace: "ImprovedAutoSlaughter.HeaderTooltipCurrentMalesYoung", done: false ),
+                ( source: "AutoSlaughterHeaderTooltipMaxMalesYoung", replace: "ImprovedAutoSlaughter.HeaderTooltipMaxMalesYoung", done: false ),
+                ( source: "AutoSlaugtherHeaderTooltipCurrentFemalesYoung", replace: "ImprovedAutoSlaughter.HeaderTooltipCurrentFemalesYoung", done: false ),
+                ( source: "AutoSlaughterHeaderTooltipMaxFemalesYoung", replace: "ImprovedAutoSlaughter.HeaderTooltipMaxFemalesYoung", done: false )
+            };
             for( int i = 0; i < codes.Count; ++i )
             {
                 // Log.Message("T:" + i + ":" + codes[i].opcode + "::" + (codes[i].operand != null ? codes[i].operand.ToString() : codes[i].operand));
-                if( codes[ i ].opcode == OpCodes.Ldstr && codes[ i ].operand.ToString() == "AnimalMaleYoung" )
+                if( codes[ i ].opcode == OpCodes.Ldstr )
                 {
-                    codes[ i ] = new CodeInstruction( OpCodes.Ldstr, "ImprovedAutoSlaughter.AnimalMaleTotal" );
-                    found1 = true;
-                }
-                if( codes[ i ].opcode == OpCodes.Ldstr && codes[ i ].operand.ToString() == "AnimalFemaleYoung" )
-                {
-                    codes[ i ] = new CodeInstruction( OpCodes.Ldstr, "ImprovedAutoSlaughter.AnimalFemaleTotal" );
-                    found2 = true;
+                    for( int j = 0; j < replace.Length; ++j )
+                    {
+                        if( !replace[ j ].done && codes[ i ].operand.ToString() == replace[ j ].source )
+                        {
+                            codes[ i ] = new CodeInstruction( OpCodes.Ldstr, replace[ j ].replace );
+                            replace[ j ].done = true;
+                            break;
+                        }
+                    }
                 }
             }
-            if(!found1 || !found2)
+            bool found = true;
+            for( int j = 0; j < replace.Length; ++j )
+                if( !replace[ j ].done )
+                    found = false;
+            if(!found)
                 Log.Error("ImprovedAutoSlaughter: Failed to patch Dialog_AutoSlaughter.DoAnimalHeader()");
             return codes;
         }
